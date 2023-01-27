d35eb0283f4bf6c07cfbc48b24d63b05601495a8
Tokko Blogger <21377149+twinnybloggers@users.noreply.github.com>
2023-01-27 10:49:00 +0700

    Add files via upload

---
+++ includes/config-example.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * config
+ * 
+ * @package BuddyEngine
+ * @author BuddyEngine
+ */
+
+// ** MySQL settings ** //
+/** The name of the database */
+define('DB_NAME', '');
+/** MySQL database username */
+define('DB_USER', '');
+/** MySQL database password */
+define('DB_PASSWORD', '');
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+/** MySQL port */
+define('DB_PORT', '3306');
+// ** System URL ** //
+define('SYS_URL', ''); // e.g (http://example.com)
+// ** i18n settings ** //
+define('DEFAULT_LOCALE', 'id_id');
+define('DEBUGGING', false);
+
